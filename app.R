2292ef38ee3af2d2730f01379a86ebbe1d07bc4f
hongyuanzhang <hzhang584@wisc.edu>
2021-03-12 14:23:25 -0600

    push

--- app.R
+++ app.R
@@ -30,16 +30,25 @@ ui <- fluidPage(
 # Server logic ----
 server <- function(input, output) {
   output$map <- renderPlot({
-    args <- switch(input$var,
-                   "Percent White" = list(counties$white, "darkgreen", "% White"),
-                   "Percent Black" = list(counties$black, "black", "% Black"),
-                   "Percent Hispanic" = list(counties$hispanic, "darkorange", "% Hispanic"),
-                   "Percent Asian" = list(counties$asian, "darkviolet", "% Asian"))
+    data <- switch(input$var, 
+                   "Percent White" = counties$white,
+                   "Percent Black" = counties$black,
+                   "Percent Hispanic" = counties$hispanic,
+                   "Percent Asian" = counties$asian)
     
-    args$min <- input$range[1]
-    args$max <- input$range[2]
+    color <- switch(input$var, 
+                    "Percent White" = "darkgreen",
+                    "Percent Black" = "black",
+                    "Percent Hispanic" = "darkorange",
+                    "Percent Asian" = "darkviolet")
     
-    do.call(percent_map, args)
+    legend <- switch(input$var, 
+                     "Percent White" = "% White",
+                     "Percent Black" = "% Black",
+                     "Percent Hispanic" = "% Hispanic",
+                     "Percent Asian" = "% Asian")
+    
+    percent_map(var = data, color = color, legend.title = legend, max = input$range[1], min = input$range[2])
   })
 }
 
